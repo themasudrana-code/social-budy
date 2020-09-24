e15e6f72db8cc1a1f079f8b3bd88840b7cd28c84
masud rana <you@example.com>
2020-09-24 15:18:44 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import Home from './components/Home/Home';
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import NotFound from './components/NotFound/NotFound';
 import Header from './components/Header/Header';
